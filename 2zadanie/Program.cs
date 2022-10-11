00c006703a574e64131ff56508e51dd1d45f3859
on14an_ <on14an@ON14AN>
2022-10-11 23:28:04 +0700

    Добавьте файлы проекта.

---
+++ 2zadanie/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите a ");
+        Double a = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите b ");
+        Double b = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Введите c ");
+        Double c = Convert.ToDouble(Console.ReadLine());
+        Double D = (b * b - 4 * a * c);
+        if (D < 0)
+            Console.WriteLine("нет решений");
+        else if (D == 0)
+            Console.WriteLine($"x = {-b / (2 * a)}");
+        else if (D > 0)
+        {
+            Console.WriteLine($"x = {-b + Math.Sqrt(D) / (2 * a)}");
+            Console.WriteLine($"x = {-b - Math.Sqrt(D) / (2 * a)}");
+        }
+
+    }
+
+}
